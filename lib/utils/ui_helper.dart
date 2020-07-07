6c3d5cc4294a884f1e9a9e431d09b3a974d5a31f
Zin Thuzar Win <zinthuzarwin@gmail.com  git config --global user.name Zin Thuzar Win>
2020-07-07 10:46:41 +0630

    Build Project Structure

---
+++ lib/utils/ui_helper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UIHelper {
+
+  static const TextStyle headerStyle = TextStyle(
+    fontSize: 35, fontWeight: FontWeight.w900, color: textColor
+  );
+  static const TextStyle subHeaderStyle = TextStyle(
+    fontSize: 16.0, fontWeight: FontWeight.w500, color: textColor
+  );
+
+  static const Color textColor = Colors.black;
+
+  static const Color backgroundColor = Colors.white;
+
+  static const Color themeColor = Color.fromRGBO(255, 0, 0, 0.5);
+}
