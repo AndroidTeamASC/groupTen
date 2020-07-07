54383003ded89b755a6cc28ef55fbcb05a4aea0a
Zin Thuzar Win <zinthuzarwin@gmail.com  git config --global user.name Zin Thuzar Win>
2020-07-07 16:06:47 +0630

    modify code

--- lib/ui/favourite_news.dart
+++ lib/ui/favourite_news.dart
@@ -10,7 +10,18 @@ class FavouriteNews extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.transparent,)
+        appBar: AppBar(backgroundColor: Colors.transparent,),
+        body: ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            final item = articles[index];
+
+            return ListTile(
+              leading: Image.network(item.urlToImage, fit: BoxFit.fill, width: 100, height: 100,),
+              title: Text(item.title),
+              subtitle: Text(item.author),
+            );
+          }),
       ),
     );
   }
